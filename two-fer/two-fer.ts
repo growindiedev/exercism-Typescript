f02d5d36e11159a511dffc1b75b389ad4e5a6cd3
jarryingnut <rabhishekranjan@gmail.com>
2023-02-21 11:21:42 +0530

    do two-fer iteration2

--- two-fer/two-fer.ts
+++ two-fer/two-fer.ts
@@ -2,13 +2,6 @@
  * This stub is provided to make it straightforward to get started.
  */
 
-export function twoFer(name?: string): string {
-  // ^                 ^   ^ this is called a return type; it's the type of the
-  // ^                 ^     value that is returned from this function
-  // ^                 ^
-  // ^                 parameters go here
-  if(!name){
-    return "One for you, one for me.";
-  } 
+export function twoFer(name = 'you'): string {
   return `One for ${name}, one for me.`
 }
